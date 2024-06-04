8d07f03152d16440e0785c725cf9ff4b542f366c
Molly Reed <mollyreeddesign@gmail.com>
2024-06-04 17:18:54 -0400

    feat: Add fallback metadata to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      <h1 className="font-body text-5xl font-bold">Hello World!</h1>
+      <h1 className="font-body text-5xl font-bold">Hello World!!</h1>
     </main>
   );
 }
